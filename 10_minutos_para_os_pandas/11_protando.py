097c0ecebca10f4c5fc680effc4b3825cfbe7e06
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-04-12 14:32:06 -0300

    plotando

---
+++ 10_minutos_para_os_pandas/11_protando.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+
+ts = ts.cumsum()
+
+ts.plot()
+plt.show()
